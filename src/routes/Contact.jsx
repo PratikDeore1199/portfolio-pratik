8d0f8c7d8bf28cb67a7f936651c2e9cdcab8c04d
PratikDeore1199 <122374744+PratikDeore1199@users.noreply.github.com>
2023-06-09 19:24:39 +0530

    done with media querries

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <Navbar/>
+      <h1>Currently working on this section</h1>
+      <Footer/>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
